b4f9da840e12ee3e76c39c1d7a707aeaf495f613
Pedro Meza Alarcon <55568096+Peter1098@users.noreply.github.com>
2023-07-04 09:17:00 -0600

    Add files via upload

---
+++ Python_peter/cadenas1.py
@@ -0,0 +1,12 @@
+cadena="Te quiero solo como amigo"
+
+#imprime los 2 primeros caracteres
+print(cadena[0:2])
+#imprime los 3 ultimos caracteres
+print(cadena[22:])
+#imprime cada 2 letras cadena[inicio:final:saltos]
+print(cadena[::2])
+#imprime cadenas al reves
+print(cadena[::-1])
+#Imprima la cadena en un sentido y en sentido inverso.
+print(cadena,cadena[::-1])
---
+++ Python_peter/debanadoString.py
@@ -0,0 +1,14 @@
+cadena="Este es un ejemplo de substring (debanado de cadenas)"
+#tamaño de la cadena
+print(len(cadena))
+#muestra solo el lugar 2 de toda la cadena
+print(cadena[2])
+#muestra de 0 a 10 de la cadena
+print(cadena[0:10])
+#muestra de 0 a 20 de la cadena
+print(cadena[:20])
+#muestra de 20 hasta el termino de la cadena
+print(cadena[20:])
+#
+print("1",cadena[-5:])
+
