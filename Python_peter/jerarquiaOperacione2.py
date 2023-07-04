1fd6b3532de8a670ba759e46a34f9339f553ac4a
Pedro Meza Alarcon <55568096+Peter1098@users.noreply.github.com>
2023-07-03 18:30:11 -0600

    Add files via upload

---
+++ Python_peter/jerarquiaOperacione2.py
@@ -0,0 +1,8 @@
+payaso=112
+muneca=75
+
+resultado=((payaso*23)+(muneca*54))
+print("un payaso pesa 112g y una muñeca 54g")
+print("cuanto pesan 23 payasos y 54 muñecas")
+resultado=((payaso*23)+(muneca*54))
+print(resultado,"g")
---
+++ Python_peter/operadoresAritmeticos.py
@@ -0,0 +1,11 @@
+print("suma 2+2=" ,2+2)
+print("resta 150-75=" ,150-75)
+print("multiplicacion 10*10=" ,10*10)
+print("division 75/9=", 75/9)
+
+print("Exponente 2^3=",2**3)# 2*2*2
+#division entera donde solo devuelte enteros
+print("division entera 75/9=",75//9)
+
+#residuo de una division
+print("el residuo de la dision 100/100=",100%100)
